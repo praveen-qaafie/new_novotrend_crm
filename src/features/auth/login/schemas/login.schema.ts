1c6b3cefa0a81e43028e6bf6ff797e62f8577c72
praveen suthar <praveen@qaafie.com>
2026-06-10 18:52:47 +0530

    feat: singup page update

--- src/features/auth/login/schemas/login.schema.ts
+++ src/features/auth/login/schemas/login.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email("Invalid email address"),
+  email: z.string().email("Please enter your email address"),
   password: z.string().min(1, "Please enter your password"),
 });
 
